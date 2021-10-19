d19cf49ab51147fa00ab2ad46b70a1aa2e69d465
Jim <92756045+rockycodemaker@users.noreply.github.com>
2021-10-19 21:41:04 +0200

    added a main function

---
+++ arduino.py
@@ -0,0 +1,40 @@
+import serial
+
+
+def find_comports():
+    # search available com ports
+    # choose first one (for now)
+    return ['COM3']
+
+class Serialcom:
+    def __init__(self, comport_name):
+        self.comport_name = comport_name
+        self.baud_rate = 9600
+        self.timeout = 1
+        self.ser = serial.Serial()
+
+    def serial_write(self, message):
+        print(message)
+        #self.ser.write(message)
+
+    def serial_read(self):
+        # read until newline and return the string
+        # self.ser.read(10)
+        return "message_received!"
+
+    def init(self):
+        connected = False
+        self.ser = serial.Serial(self.comport_name, self.baud_rate, timeout=self.timeout) # open comport connection
+
+        # send the magic message
+        if self.ser.is_open:
+            self.serial_write('magic_message!')
+            answer = self.serial_read()
+            if answer == "message_received!":  # check for the magic answer
+                connected = True
+            else:
+                connected = False
+        return connected
+
+    def close_comport(self):
+        self.ser.close()
---
+++ main.py
@@ -0,0 +1,15 @@
+# this file will contain the main code
+import arduino
+
+
+def Main():
+    # initialize serial connection
+    port_names = arduino.find_comports()                # get a list op port names
+    port_name = port_names[0]                           # choose a port
+    micro_controller = arduino.Serialcom(port_name)     # create a 'Serialcom' class to setup serial communication
+    if micro_controller.init():                         # initialize the serial communication
+        micro_controller.close_comport()
+
+
+if __name__ == '__main__':
+    Main()
